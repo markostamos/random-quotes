107f1d582b7db5cd8c059496e7fb0d45d497f102
Markostamos <gimarkostamos@hotmail.com>
2018-10-20 20:25:42 +0300

    Updates

---
+++ precache-manifest.18d0c25f5a5994fb98da92bee198aab0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4554ae4c421f369a667",
+    "url": "/random-quotes/static/js/runtime~main.a4554ae4.js"
+  },
+  {
+    "revision": "0da7da14e1209d15c2ff",
+    "url": "/random-quotes/static/js/main.0da7da14.chunk.js"
+  },
+  {
+    "revision": "83b3f9b8655729265301",
+    "url": "/random-quotes/static/js/1.83b3f9b8.chunk.js"
+  },
+  {
+    "revision": "0da7da14e1209d15c2ff",
+    "url": "/random-quotes/static/css/main.2508cdee.chunk.css"
+  },
+  {
+    "revision": "4c481268eaa2745a6a8904593adb0041",
+    "url": "/random-quotes/index.html"
+  }
+];
