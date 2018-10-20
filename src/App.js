e98391d044c1c0e97da1ef7a13e1830fe4b0d182
Markostamos <gimarkostamos@hotmail.com>
2018-10-20 20:13:41 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+const pallete=["#008E9B","#008F7A","#4B4453","#00C9A7","#C34A36","#B25B00","#402E32","#812A00"];
 class App extends Component {
+  state={
+    text:undefined,
+    author:undefined,
+    color:undefined
+  }
+  componentWillMount(){
+    this.getQuote();
+  }
+  getQuote= async ()=>{
+   const API_call= await fetch("https://talaikis.com/api/quotes/random/");
+   const data=await API_call.json();
+   this.setState((state)=>({
+     text:data.quote,
+     author:data.author,
+     color:pallete[Math.floor(Math.random()*pallete.length)]
+   }));
+   
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" style={{backgroundColor:this.state.color}}>
+        <div className="card" id="quote-box">
+          <div className="mycontainer">
+          <i class="fas fa-quote-left"></i><p id="text" style={{color:this.state.color}}>{this.state.text}</p><i class="fas fa-quote-right"></i>
+            <p id="author" style={{color:this.state.color}}>{'-'+this.state.author}</p>
+          </div>
+          <hr/>
+          <div class="footer">
+        
+           <a id="tweeet-quote" href={"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="+ encodeURIComponent('"' +this.state.text + '" ' +'\n'+'-'+ this.state.author)} target="_blank"> <i class="fab fa-twitter myicon" style={{backgroundColor:this.state.color}}></i></a>
+            <button id="new-quote" style={{backgroundColor:this.state.color}} onClick={this.getQuote}>Generate Quote</button>
+          </div>
+        </div>
       </div>
     );
   }
